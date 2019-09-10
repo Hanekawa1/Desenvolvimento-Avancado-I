710c8597a45fa1344cbeda9e9601cb24f2376e66
Wender lucas <wenderlucas.souza@hotmail.com>
2019-09-10 08:53:14 -0300

    Atualizando projeto

--- APIOrientacao/APIOrientacao/Controllers/PessoaController.cs
+++ APIOrientacao/APIOrientacao/Controllers/PessoaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using APIOrientacao.Api.Request;
 using APIOrientacao.Api.Response;
+using APIOrientacao.Data;
 using APIOrientacao.Data.Context;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,8 +25,40 @@ namespace APIOrientacao.Controllers
         [ProducesResponseType(400)] //Em caso de erro
         public IActionResult Post([FromBody] PessoaRequest pessoaRequest)
         {
+            var pessoa = new Pessoa
+            {
+                Nome = pessoaRequest.Nome
+            };
+
+            contexto.Pessoa.Add(pessoa);
+            contexto.SaveChanges();
+
+            var pessoaRetorno = contexto.Pessoa.Where(x => x.IdPessoa == pessoa.IdPessoa).FirstOrDefault();
+
             PessoaResponse response = new PessoaResponse();
+
+            if (pessoaRetorno != null)
+            {
+                response.Idpessoa = pessoaRetorno.IdPessoa;
+                response.Nome = pessoaRetorno.Nome;
+            }
+
             return StatusCode(200, response);
         }
+
+        [HttpGet("{idPessoa}")]
+        [ProducesResponseType(typeof(PessoaResponse), 200)]
+        [ProducesResponseType(400)]
+        public IActionResult Get(int idPessoa)
+        {
+            var pessoa = contexto.Pessoa.FirstOrDefault(x => x.IdPessoa == idPessoa);
+
+            return StatusCode(pessoa == null ? 404 : 200, new PessoaResponse
+            {
+                Idpessoa = pessoa == null ? -1 : pessoa.IdPessoa,
+                Nome = pessoa == null ? "Pessoa não encontrada" : pessoa.Nome
+            });
+        }
+
     }
 }
